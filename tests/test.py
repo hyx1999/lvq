76a43e4e02c3d0e3d0fd637ea2113a21871a6dd5
hyx <hyx2147483647@gmail.com>
2025-04-07 23:55:07 +0800

    [Dev] Update test

--- tests/test.py
+++ tests/test.py
@@ -1,34 +1,12 @@
 import torch
-from lvq.modules.tunable_lut_linear import TunableLutLinear
-
-# class Args:
-    
-#     def __init__(self):
-#         self.iteration = 0
-#         self.train_iters = 1000
-
-# args = Args()
-
-# fc = TunableLutLinear(
-#     128,
-#     128,
-#     args,
-#     num_lut=3,
-#     lut_size=16,
-#     vec_size=4,
-#     device="cuda",
-#     dtype=torch.bfloat16
-# )
-
-# x = torch.randn((16, 128)).to(torch.bfloat16).cuda()
-# y: torch.Tensor = fc(x)
-# print("y = \n{}".format(y))
-# y.mean().backward()
-# print("grad = \n{}".format(fc.weight.gate.grad))
-
 import torch.nn.functional as F
-x = torch.zeros((16,))
-x[0] = 1
-print(x)
-print(F.softmax(x))
-print(F.gumbel_softmax(F.softmax(x) * 150, tau=4))
+
+# x = torch.randn((4,))
+# x_scale = x.pow(2).mean().sqrt()
+# x = x / (x_scale + 1e-5)
+# print(x)
+# print(F.gumbel_softmax(x, tau=4))
+
+x = torch.empty((4,))
+torch.nn.init.normal_(x, std=1)
+print(x * 0.1)
---
+++ tests/test_quant_gptq.py
@@ -0,0 +1,63 @@
+import torch
+from transformers import LlamaForCausalLM
+from lvq.quant.quantize import quant_gptq
+
+# model: LlamaForCausalLM = LlamaForCausalLM.from_pretrained("/data/models/Llama-3.2-1B")
+# state_dict = model.state_dict()
+# weight = state_dict["model.layers.0.self_attn.q_proj.weight"]
+# torch.save(weight, "misc/weight.pt")
+
+def RTN(weight: torch.Tensor, group_size: int):
+    max_int = 3
+    min_int = -4
+    out_features = weight.shape[0]
+    in_features = weight.shape[1]
+    weight = weight.reshape(out_features, in_features // group_size, group_size)
+    scales = weight.abs().max(dim=-1, keepdim=True).values / max_int
+
+    qweight = torch.clamp(torch.round(weight / scales), min_int, max_int)
+    new_weight = qweight * scales
+    return new_weight.reshape(out_features, in_features)
+
+
+class Args:
+    
+    def __init__(self):
+        self.num_lut = 3
+        self.lut_size = 16
+        self.vec_size = 4
+        self.group_size = 128
+        self.train_iters = 0
+        self.lr = 1e-4
+        self.min_lr = 1e-6
+        self.gptq_percdamp = 0.01
+        self.gptq_blocksize = 128
+
+args = Args()
+weight: torch.Tensor = torch.load("misc/weight.pt").to("cuda:1")
+hessian = torch.eye(weight.shape[1]).type_as(weight).to(weight.device)
+
+rtn_weight = RTN(weight.clone(), args.group_size)
+print("dw:\n{}".format(rtn_weight - weight))
+print("max(abs(dw)): {}".format((rtn_weight - weight).abs().max()))
+print("mean(abs(dw)): {}".format((rtn_weight - weight).abs().mean()))
+print("norm(dw): {}".format((rtn_weight - weight).norm().item()))
+d_w = rtn_weight - weight
+print("trace: {}".format(torch.trace(d_w @ hessian @ d_w.T)))
+
+_, lvq_weight = quant_gptq.reconstruct_weight_gptq(
+    args,
+    weight.clone(),
+    hessian,
+    args.num_lut,
+    args.lut_size,
+    vec_size=args.vec_size,
+    group_size=args.group_size,
+    return_weight=True,
+)
+print("dw:\n{}".format(lvq_weight - weight))
+print("max(abs(dw)): {}".format((lvq_weight - weight).abs().max()))
+print("mean(abs(dw)): {}".format((lvq_weight - weight).abs().mean()))
+print("norm(dw): {}".format((lvq_weight - weight).norm().item()))
+d_w = lvq_weight - weight
+print("trace: {}".format(torch.trace(d_w @ hessian @ d_w.T)))
