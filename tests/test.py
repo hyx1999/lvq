22c5809917b87d64f7014acf20859d0abe68e094
hyx <hyx2147483647@gmail.com>
2025-04-15 19:49:34 +0800

    [Dev] Update tests

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,7 @@
+import math
 import torch
 import torch.nn.functional as F
+import fast_hadamard_transform
 
 # x = torch.randn((4,))
 # x_scale = x.pow(2).mean().sqrt()
@@ -7,6 +9,15 @@ import torch.nn.functional as F
 # print(x)
 # print(F.gumbel_softmax(x, tau=4))
 
-x = torch.empty((4,))
-torch.nn.init.normal_(x, std=1)
-print(x * 0.1)
+# x = torch.empty((4,))
+# torch.nn.init.normal_(x, std=1)
+# print(x * 0.1)
+
+d = 128
+x = torch.randn((d,), dtype=torch.float16, device="cuda")
+org_x = x
+print(x)
+x = fast_hadamard_transform.hadamard_transform(x, 1 / math.sqrt(d))
+x = fast_hadamard_transform.hadamard_transform(x, 1 / math.sqrt(d))
+print(x)
+print((x - org_x).abs().mean())
